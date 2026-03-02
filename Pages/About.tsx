642757e338c619e073b6cf7686c833b62f1f240d
debbiebli262-byte <debbiebli262@gmail.com>
2026-03-02 20:22:06 +0200

    Update About.tsx

--- Pages/About.tsx
+++ Pages/About.tsx
@@ -4,7 +4,6 @@ import { useI18n } from "../i18n";
 const About: React.FC = () => {
   const { t } = useI18n();
 
-  // שימוש בקובץ המקומי מתיקיית public
   const logoUrl = "/logo.png";
 
   return (

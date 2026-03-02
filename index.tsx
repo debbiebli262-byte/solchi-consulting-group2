a144bbc3c0808bef0eb5e2435d92958dfb58bf88
debbiebli262-byte <debbiebli262@gmail.com>
2026-03-02 21:54:58 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
-import "./index.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { LanguageProvider } from './i18n';
+import "./index.css";
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
